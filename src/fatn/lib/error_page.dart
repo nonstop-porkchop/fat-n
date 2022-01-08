d95dd65770517db71fc4b9296339358594466113
porkchop <missespounder@gmail.com>
2022-01-08 19:56:31 +1100

    Fleshed out error page
    I'm gonna be looking at it a lot so I may as well make it look good.

--- src/fatn/lib/error_page.dart
+++ src/fatn/lib/error_page.dart
@@ -1,5 +1,6 @@
-
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ErrorPage extends StatefulWidget {
   const ErrorPage({Key? key}) : super(key: key);
@@ -9,8 +10,57 @@ class ErrorPage extends StatefulWidget {
 }
 
 class _ErrorPageState extends State<ErrorPage> {
+  static const _darkThemeTextStyle = TextStyle(color: Colors.white);
+  static const _issuesPageUrl =
+      'https://github.com/nonstop-porkchop/fat-n/issues';
+  var _urlOpenFailureMessage = "";
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Error!"));
+    return Scaffold(
+      body: Center(
+          child: Column(
+        children: [
+          const Text("Error",
+              style: TextStyle(fontSize: 34, color: Colors.redAccent)),
+          const SizedBox(height: 10),
+          const Image(image: AssetImage("images/does_not_compute.png")),
+          const SizedBox(height: 10),
+          RichText(
+            text: TextSpan(
+              children: [
+                const TextSpan(
+                    text:
+                        "An error occurred, please report (or fix) your issue on ",
+                    style: _darkThemeTextStyle),
+                TextSpan(
+                    text: "our GitHub page",
+                    style: const TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () async {
+                        if (await canLaunch(_issuesPageUrl)) {
+                          await launch(_issuesPageUrl);
+                        } else {
+                          setState(() {
+                            _urlOpenFailureMessage =
+                                "Failed to open link automatically, here's the link for you: " +
+                                    _issuesPageUrl;
+                          });
+                        }
+                      }),
+                const TextSpan(text: ".", style: _darkThemeTextStyle)
+              ],
+            ),
+          ),
+          SelectableText(
+            _urlOpenFailureMessage,
+            style: const TextStyle(color: Colors.redAccent),
+          )
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+      )),
+    );
   }
 }
